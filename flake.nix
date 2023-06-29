13467f46562e7bdf539ec99186c140f12314783a
Andrew Plaza <aplaza@liquidthink.net>
2023-06-29 12:45:44 -0400

    flake

--- flake.nix
+++ flake.nix
@@ -21,6 +21,7 @@
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
         modules = [ ./home.nix ];
+        extraSpecialArgs = { inherit inputs; };
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
