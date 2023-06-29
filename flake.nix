4d12d62233142c22ddcbcceb25a73112d2ef4c31
Andrew Plaza <aplaza@liquidthink.net>
2023-06-29 12:24:58 -0400

    changes

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
+      # Home manager has its own packages that can be used, but if we tell home manager to use nixpkgs, it will build and set everything up with packages in nix package manager.
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -17,14 +18,15 @@
         inherit system;
 	config.allowUnfree = true;
       };
+      lib = nixpkgs.lib;
     in {
       homeConfigurations."insipx" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+        inherit system pkgs;
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
         modules = [ ./home.nix ];
-
+       
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
       };
