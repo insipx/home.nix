f0053835dea8b6e606565b532e14904a4ecd27fe
Andrew Plaza <aplaza@liquidthink.net>
2023-06-29 12:47:39 -0400

    remove inputs

--- flake.nix
+++ flake.nix
@@ -21,7 +21,6 @@
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
         modules = [ ./home.nix ];
-        extraSpecialArgs = { inherit inputs; };
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
