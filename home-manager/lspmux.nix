587d00c3e6f06f01f1a51f8d049f41f071df920a
Andrew Plaza <github@andrewplaza.dev>
2026-02-21 15:52:35 -0500

    overhaul keybinds

--- home-manager/lspmux.nix
+++ home-manager/lspmux.nix
@@ -1,31 +1,28 @@
 { pkgs, ... }:
+let
+  rust-analyzer-wrapped = pkgs.writeShellScriptBin "rust-analyzer" ''
+    exec ${pkgs.direnv}/bin/direnv exec "$PWD" \
+      ${pkgs.rust-analyzer-nightly}/bin/rust-analyzer "$@"
+  '';
+in
 {
-
-  systemd.user.services.lspmux =
-    # let
-    #   rust-analyzer-wrapped = pkgs.writeShellScriptBin "rust-analyzer" ''
-    #     exec ${pkgs.direnv}/bin/direnv exec "$PWD" \
-    #       ${pkgs.rust-analyzer-nightly}/bin/rust-analyzer "$@"
-    #   '';
-    # in
-    {
-      enable = true;
-      # after = [ "network.target" ];
-      # wantedBy = [ "default.target" ];
-      #path = with pkgs; [
-      #  rust-analyzer-wrapped
-      #  gcc
-      #  mold
-      #  direnv
-      #  inputs.fenix.packages.${pkgs.system}.minimal.toolchain
-      #  nix
-      #];
-      Unit = {
-        Description = "Language server multiplexer";
-      };
-      Service = {
-        Type = "simple";
-        ExecStart = "${pkgs.lspmux}/bin/lspmux server";
-      };
+  systemd.user.startServices = "sd-switch";
+  systemd.user.services.lspmux = {
+    Unit = {
+      Description = "Language server multiplexer";
     };
+    Service = {
+      Type = "simple";
+      Environment = "PATH=${pkgs.lib.makeBinPath [
+        rust-analyzer-wrapped
+        pkgs.direnv
+        pkgs.nix
+        pkgs.coreutils
+      ]}";
+      ExecStart = "${pkgs.lspmux}/bin/lspmux server";
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
 }
